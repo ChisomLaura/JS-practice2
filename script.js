faeb86c3cafd9acbd7fd7af998ef6cfbeafed2ea
Chisom Laura <102640478+ChisomLaura@users.noreply.github.com>
2022-06-10 09:06:06 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var userName = prompt("What is your name?");
+var birthYear = prompt("What year were you born? (ex 1990)");
+
+console.log(
+  `Your name is ${userName} and you're ${2022 - birthYear} years old.`
+);
